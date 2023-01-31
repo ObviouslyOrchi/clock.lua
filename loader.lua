e0c6aaf586dde6d3bd082230b6e4749f06bdf7e6
Tokkenik <86359449+Tokkenik@users.noreply.github.com>
2023-01-31 23:49:24 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,21 @@
 getgenv().LoadedFromLoader = true
+local original;
+original = hookmetamethod(game:GetService("Players").LocalPlayer.Character.Humanoid, "__index", function(self, index)
+    if original(self, "Name") == "Humanoid" and index == "WalkSpeed" then
+        return 16
+    end
+    return original(self, index)
+end);
+local original;
+original = hookmetamethod(game:GetService("Players").LocalPlayer.Character.Humanoid, "__index", function(self, index)
+    if original(self, "Name") == "Humanoid" and index == "JumpPower" then
+        return 16
+    end
+    return original(self, index)
+end);
+
+game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = 100
+game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = 100
 if game.PlaceId == 5865858426 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tokkenik/clock.lua/main/main.lua"))()
 else
