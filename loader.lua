0bd80667ae0edf077c18b6075b42d55eff967c5e
Tokkenik <86359449+Tokkenik@users.noreply.github.com>
2023-02-02 23:18:02 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,26 +1,29 @@
-getgenv().LoadedFromLoader = true
+fromloader = true
 
-spawn(function()
-    local mt = getrawmetatable(game)
-    local hum = game:GetService("Players").LocalPlayer.Character.Humanoid
-    local old = mt.__index
-    setreadonly(mt, false)
-    mt.__index = newcclosure(function(instance, index)
-        if instance == hum and index == "WalkSpeed" then
-            return 16
-        else
-            return old(instance, index)
-        end
+if not alreadybypassed then
+    spawn(function()
+        local mt = getrawmetatable(game)
+        local hum = game:GetService("Players").LocalPlayer.Character.Humanoid
+        local old = mt.__index
+        setreadonly(mt, false)
+        mt.__index = newcclosure(function(instance, index)
+            if instance == hum and index == "WalkSpeed" then
+                return 16
+            else
+                return old(instance, index)
+            end
+        end)
+        mt.__index = newcclosure(function(instance, index)
+            if instance == hum and index == "JumpPower" then
+                return 50
+            else
+                return old(instance, index)
+            end
+        end)
+        setreadonly(mt, true)
+        alreadybypassed = true
     end)
-    mt.__index = newcclosure(function(instance, index)
-        if instance == hum and index == "JumpPower" then
-            return 50
-        else
-            return old(instance, index)
-        end
-    end)
-    setreadonly(mt, true)
-end)
+end
 
 if game.PlaceId == 5865858426 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tokkenik/clock.lua/main/main.lua"))()
