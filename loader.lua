2b7e5252acc401cf01368578f0c5bcfd4760f7cb
Tokkenik <86359449+Tokkenik@users.noreply.github.com>
2023-02-07 20:48:32 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,33 +1,5 @@
 getgenv().fromloader = true
 
-
-if not newcclosure and fireclickdetector and firetouchinterest then
-    game:GetService("Players").LocalPlayer:Kick("get a better executor pooron")
-end
-if not getgenv().alreadybypassed then
-    spawn(function()
-        local mt = getrawmetatable(game)
-        local hum = game:GetService("Players").LocalPlayer.Character.Humanoid
-        local old = mt.__index
-        setreadonly(mt, false)
-        mt.__index = newcclosure(function(instance, index)
-            if instance == hum and index == "WalkSpeed" then
-                return 16
-            else
-                return old(instance, index)
-            end
-        end)
-        mt.__index = newcclosure(function(instance, index)
-            if instance == hum and index == "JumpPower" then
-                return 50
-            else
-                return old(instance, index)
-            end
-        end)
-        setreadonly(mt, true)
-        getgenv().alreadybypassed = true
-    end)
-end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/finayv2/UAB/main/Main.lua"))()
 
 if game.PlaceId == 5865858426 then
