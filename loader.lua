ead0bcad2c49ba89a77396300fb3c1394126f57d
Tokkenik <86359449+Tokkenik@users.noreply.github.com>
2023-02-07 21:07:08 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,8 @@
 getgenv().fromloader = true
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/finayv2/UAB/main/Main.lua"))()
+spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/finayv2/UAB/main/Main.lua"))()
+end)
 
 if game.PlaceId == 5865858426 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tokkenik/clock.lua/main/rt2.lua"))()
