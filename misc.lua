958086103a3968570986f85b6177898af0764e5d
Tokkenik <86359449+Tokkenik@users.noreply.github.com>
2023-03-03 02:05:29 +0100

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -1,4 +1,3 @@
-if not getgenv().fromloader then game:GetService("Players").LocalPlayer:Kick("Please use the loader instead of the main script, thanks. .gg/GPVqBUmPb3") end
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
 
 local Window = Rayfield:CreateWindow({
@@ -31,26 +30,86 @@ function removech()
 	end
 end
 
+if getgenv().infjump == nil then
+    game:GetService("UserInputService").JumpRequest:connect(function()
+    	if getgenv().infjump == true then
+            game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+    	end
+    end)
+end
+
 local Bodyexpander = Window:CreateTab("Body Expander")
+local LocalPlayer = Window:CreateTab("LocalPlayer")
+local Misc = Window:CreateTab("Misc")
 Bodyexpander:CreateSection("Put body type to 100% in avatar settings, won't work if the game doesn't support body type.")
 
 Bodyexpander:CreateButton({Name = "Expand BodyProportionScale",Interact = "Button",Callback = function()
     pcall(removech)
     game:GetService("Players").LocalPlayer.Character.Humanoid:FindFirstChild("BodyProportionScale"):Destroy()
 end})
+
 Bodyexpander:CreateButton({Name = "Expand HeadScale",Interact = "Button",Callback = function()
     pcall(removech)
     game:GetService("Players").LocalPlayer.Character.Humanoid:FindFirstChild("HeadScale"):Destroy()
 end})
+
 Bodyexpander:CreateButton({Name = "Expand BodyWidthScale",Interact = "Button",Callback = function()
     pcall(removech)
 game:GetService("Players").LocalPlayer.Character.Humanoid:FindFirstChild("BodyWidthScale"):Destroy()
 end})
+
 Bodyexpander:CreateButton({Name = "Expand BodyDepthScale",Interact = "Button",Callback = function()
     pcall(removech)
     game:GetService("Players").LocalPlayer.Character.Humanoid:FindFirstChild("BodyDepthScale"):Destroy()
 end})
+
 Bodyexpander:CreateButton({Name = "Expand BodyHeightScale",Interact = "Button",Callback = function()
     pcall(removech)
     game:GetService("Players").LocalPlayer.Character.Humanoid:FindFirstChild("BodyHeightScale"):Destroy()
+end})
+
+LocalPlayer:CreateSlider({Name = "Walk Speed",Range = {0, 100},Increment = 1,CurrentValue = 16,Callback = function(Value)
+    game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = Value
+end})
+
+LocalPlayer:CreateSlider({Name = "Jump Power",Range = {0, 100},Increment = 1,CurrentValue = 50,Callback = function(Value)
+    game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = Value
+end})
+
+LocalPlayer:CreateSlider({Name = "Gravity",Range = {0, 196.2},Increment = 0.2,CurrentValue = 196.2,Callback = function(Value)
+    game:GetService("Workspace").Gravity = Value
+end})
+
+LocalPlayer:CreateToggle({Name = "Toggle Infinite Jump",CurrentValue = false,Callback = function(Value)
+    getgenv().infjump = Value
+end})
+
+Misc:CreateButton({Name = "Fps Booster",Interact = "Button",Callback = function()
+    game.Lighting.GlobalShadows = false
+    game.Lighting.FogEnd = (9e9 * 9e9)
+    sethiddenproperty(game.Lighting, "Technology", 0)
+    settings().Rendering.QualityLevel = "Automatic"
+    for i, v in pairs(game:GetDescendants()) do
+        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") or v:IsA("UnionOperation") or v:IsA("WedgePart") then
+            v.Material = "Plastic"
+            v.Reflectance = 0
+        elseif v:IsA("Decal") or v:IsA("Texture") then
+            v:Remove()
+        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+            v.Lifetime = NumberRange.new(0)
+        elseif v:IsA("Explosion") then
+            v.BlastPressure = 1
+            v.BlastRadius = 1
+        elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") then
+            v:Remove()
+        elseif v:IsA("MeshPart") then
+            v.Material = "Plastic"
+            v.Reflectance = 0
+            v.TextureID = 0
+        elseif v:IsA("Terrain") then
+            v.Transparency = 1
+        elseif v:IsA("BlurEffect") or v:IsA("SunRaysEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("BloomEffect") or v:IsA("DepthOfFieldEffect") then
+            v.Enabled = false
+        end
+    end
 end})
